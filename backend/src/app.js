5e40107b78fb5cf22ea1786af175fae7bd490c1d
Anik Adhikari <developwithanik@gmail.com>
2025-01-27 13:47:18 +0530

    add error handling middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,14 @@ import morgan from 'morgan';
 import logger from './utils/logger.js';
 
 // internal imports
-import authRouter from './routes/auth.route.js';
+import { ApiError } from './utils/ApiError.js';
 import { API_V } from './utils/constants.js';
 
 // app initialization
 const app = express();
 
+// middlewares
+app.use(express.json());
 
 // logging with morgan
 (() => {
@@ -29,10 +31,33 @@ const app = express();
             },
         })
     );
-})()
+})();
 
-// middlewares
+// routes import
+import authRouter from './routes/auth.route.js';
 
+// routes declaration
 app.use(API_V + '/auth', authRouter);
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    console.log(err);
+
+    if (err instanceof ApiError) {
+        return res.status(err.statusCode).json({
+            statusCode: err.statusCode,
+            success: err.success,
+            message: err.message,
+            errors: err.errors,
+            // stack: err.stack
+        });
+    }
+
+    console.error(err); // Log the error
+    res.status(500).json({
+        success: false,
+        message: 'An unexpected error occurred',
+    });
+});
 
 export default app;
